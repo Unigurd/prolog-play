388280b0b2dab39b109939c71e1a2beac5007164
Sigurd Dam Sonniks <lsn777@alumni.ku.dk>
2020-11-22 00:27:54 +0100

    Bools and if-expressions

--- arith.pl
+++ arith.pl
@@ -1,10 +1,22 @@
-parse(T,S) :- let(T,S,[]).
+:- op(920,fy, *).
+*_.
+?- use_module(library(clpfd)).
+
+parse(T,S) :- top(T,S,[]).
+
+top(L) --> let(L).
+top(I) --> if(I).
+top(E) --> expr(E).
+
+if(if(E0,E1,E2)) -->
+    [if],   top(E0),
+    [then], top(E1),
+    [else], top(E2).
 
 let(let(V,E0,E1)) -->
     [let], var(var(V)),
-    [=],   let(E0),
-    [in],  let(E1).
-let(E) --> expr(E).
+    [be],   top(E0),
+    [in],  top(E1).
 
 expr(T) --> simple_expr(T).
 expr(R) -->
@@ -43,7 +55,11 @@ term(binop(O,X,Y)) -->
 
 simple_expr(V) --> var(V).
 simple_expr(N) --> num(N).
-simple_expr(unit(L)) --> [<],let(L),[>].
+simple_expr(B) --> bool(B).
+simple_expr(unit(L)) --> [<],top(L),[>].
+
+bool(bool(true))  --> [true].
+bool(bool(false)) --> [false].
 
 var(var(a)) --> [a].
 var(var(b)) --> [b].
@@ -54,26 +70,53 @@ var(var(y)) --> [y].
 %% number/1 cannot be used to generate.
 num(num(X),[X|Y],Y) :- number(X).
 
-binop(+) --> [+].
-binop(-) --> [-].
-binop(*) --> [*].
-binop(/) --> [/].
+binop(+)   --> [+].
+binop(-)   --> [-].
+binop(*)   --> [*].
+binop(/)   --> [/].
+binop(=)   --> [=].
+binop(and) --> [and].
+binop(or)  --> [or].
 
-binopdict(*,left,2).
-binopdict(/,left,2).
-binopdict(+,left,1).
-binopdict(-,left,1).
-
+binopdict(*,   left, 5).
+binopdict(/,   left, 5).
+binopdict(+,   left, 4).
+binopdict(-,   left, 4).
+binopdict(=,   left, 3).
+binopdict(and, left, 2).
+binopdict(or,  left, 1).
 
 
 lookup([(Var-Val)|_], Var, Val).
 lookup([_|Vtab], Var, Val) :-
     lookup(Vtab,Var,Val).
 
-arith(+,X,Y,R) :- R is X + Y.
-arith(-,X,Y,R) :- R is X - Y.
-arith(*,X,Y,R) :- R is X * Y.
-arith(/,X,Y,R) :- R is X / Y.
+arith(+,num(X),num(Y),num(R)) :- R is X + Y.
+arith(-,num(X),num(Y),num(R)) :- R is X - Y.
+arith(*,num(X),num(Y),num(R)) :- R is X * Y.
+arith(/,num(X),num(Y),num(R)) :- R is X / Y.
+arith(and, bool(X), bool(Y), bool(R)) :- and(X,Y,R).
+arith(or,  bool(X), bool(Y), bool(R)) :- or(X,Y,R).
+arith(=,   bool(X), bool(Y), bool(R)) :- eq(bool(X),bool(Y),bool(R)).
+arith(=,   num(X),  num(Y),  bool(R))  :- eq(num(X),num(Y),bool(R)).
+
+and(true,  true,  true).
+and(true,  false, false).
+and(false, true,  false).
+and(false, false, false).
+
+or(true,  true,  true).
+or(true,  false, true).
+or(false, true,  true).
+or(false, false, false).
+
+eq(num(X), num(X), bool(true)).
+eq(num(X), num(Y), bool(false)) :-
+    X #\= Y.
+
+eq(bool(X), bool(X), bool(true)).
+eq(bool(X), bool(Y), bool(false)) :-
+    X \= Y.
 
 eval(E,Res) :- eval([],E,Res).
 
@@ -81,16 +124,32 @@ eval(Vtab, let(V,E0,E1),Res) :-
     eval(Vtab,E0,VRes),
     eval([(V-VRes)|Vtab], E1, Res).
 
+
 eval(Vtab, binop(Op,X0,Y0),Res) :-
     eval(Vtab, X0, X1),
     eval(Vtab, Y0, Y1),
     arith(Op,X1,Y1,Res).
 
-eval(_, num(X),X).
+eval(_, num(X),num(X)).
+
+eval(_, bool(X),bool(X)).
+
+eval(Vtab, var(V), Res) :-
+    lookup(Vtab,V,Res).
 
 eval(Vtab, unit(E), R) :-
     eval(Vtab, E, R).
 
+eval(Vtab, if(E0,E1,E2), Res) :-
+    eval(Vtab,E0,B),
+    eval_if(Vtab,B,E1,E2,Res).
 
-eval(Vtab, var(V), Res) :-
-    lookup(Vtab,V,Res).
+eval_if(Vtab, bool(true), E1, _, Res) :-
+    eval(Vtab, E1, Res).
+
+eval_if(Vtab, bool(false), _, E2, Res) :-
+    eval(Vtab, E2, Res).
+
+interpret(I,R) :-
+    parse(T,I),
+    eval(T,R).
